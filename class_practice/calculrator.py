f37b581ef5ad856b9bc3c64a420a60c932f46093
Kim GyeongMin <gyeongmin951@naver.com>
2022-09-14 14:48:20 +0900

    Cal 클래스 변경(함수이름, 오류해결)
    reaction_history -> again_history
    cal 함수에 op를 함수로 변경해줌으로 인한 오류를 이전으로 돌림
    cal 함수에 is_success를 추가함으로써 정상 출력 내역만 저장

--- class_practice/calculrator.py
+++ class_practice/calculrator.py
@@ -7,18 +7,25 @@ class Cal:
         '/' : lambda x,y: x/y
     }
     def cal(self, num1, op, num2):
+        is_success = True
         try:
             num1,num2 = int(num1),int(num2)
-            op = self.op_dict[op]
-            self.history.append((num1,op,num2))
-            return op(num1,num2)
+            return self.op_dict[op](num1,num2)
         except ValueError:
+            is_success = False
             return '숫자 연산자 숫자로 입력해주세요.'
         except KeyError:
+            is_success = False
             return '+-*/ 중 하나의 연산자를 입력해주세요.'
         except ZeroDivisionError:
+            is_success = False
             return '0으로는 못 나눕니다.'
-    def reaction_history(self, idx):
-        if abs(idx-1) > len(self.history):
-            return 'out of range'
-        return self.cal(*self.history[idx-1])
+        finally:
+            if is_success:
+                self.history.append((num1,op,num2))
+
+    def again_history(self, idx):
+        try:
+            return self.cal(*self.history[idx])
+        except IndexError:
+            return '내역의 범위 밖입니다.'
