a0ea22f6a05dc653baea7f0bbd97695857213df5
Kim GyeongMin <gyeongmin951@naver.com>
2022-09-14 14:34:02 +0900

    예외처리, 컴프리헨션, filter 연습

--- class_practice/calculrator.py
+++ class_practice/calculrator.py
@@ -7,8 +7,17 @@ class Cal:
         '/' : lambda x,y: x/y
     }
     def cal(self, num1, op, num2):
-        self.history.append((num1,op,num2))
-        return self.op_dict[op](num1,num2)
+        try:
+            num1,num2 = int(num1),int(num2)
+            op = self.op_dict[op]
+            self.history.append((num1,op,num2))
+            return op(num1,num2)
+        except ValueError:
+            return '숫자 연산자 숫자로 입력해주세요.'
+        except KeyError:
+            return '+-*/ 중 하나의 연산자를 입력해주세요.'
+        except ZeroDivisionError:
+            return '0으로는 못 나눕니다.'
     def reaction_history(self, idx):
         if abs(idx-1) > len(self.history):
             return 'out of range'
