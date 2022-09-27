042737e33a18e5ff1a9931407dc52eacbaba2ddf
Kim GyeongMin <gyeongmin951@naver.com>
2022-09-27 09:28:37 +0900

    add admin func and modify model attr

--- django-mornig-test/spartatest/introduce/models.py
+++ django-mornig-test/spartatest/introduce/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class AccessLog(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     location = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f'{self.location}에 {self.created_at}에 접속함'
