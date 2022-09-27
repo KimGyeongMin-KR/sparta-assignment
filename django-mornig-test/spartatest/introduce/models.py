8cba22680bb6425537b82192dc087666303c6b5b
Kim GyeongMin <gyeongmin951@naver.com>
2022-09-27 09:16:58 +0900

    쪽지 시험 : 로그 남기기

--- django-mornig-test/spartatest/introduce/models.py
+++ django-mornig-test/spartatest/introduce/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class AccessLog(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    location = models.CharField(max_length=32)
