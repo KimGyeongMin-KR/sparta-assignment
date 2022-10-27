ca7b6d865a67fe7f3a57363c03d81d848fb7eb71
Kim GyeongMin <gyeongmin951@naver.com>
2022-10-27 09:18:19 +0900

    Update : 아침 퀴즈 simpleJwt 인증

--- django-mornig-test/sparta/sparta/settings.py
+++ django-mornig-test/sparta/sparta/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 
     'rest_framework',
+    'rest_framework_simplejwt',
 
 
 
@@ -131,3 +132,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
